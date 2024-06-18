0ea3ad2b5cb459a2f4ab42f0bcbf5ad3586311d5
DanilSemanovgg1 <144227090+DanilSemanovgg1@users.noreply.github.com>
2024-06-18 11:20:03 +0300

    Add files via upload

---
+++ A.pas
@@ -0,0 +1,16 @@
+﻿Unit A; 
+uses GraphABC;
+Procedure ris(x1,y1,x2,y2,k:integer);
+begin
+     If k>0 then 
+       begin
+          var xn:=(x1+x2) div 
+          2 +(y2-y1) div 2;
+          var yn:=(y1+y2) div 2 -(x2-x1) div 2;
+          ris(x1,y1,xn,yn,k-1);
+          ris(x2,y2,xn,yn,k-1)
+     end
+     else line(x1,y1,x2,y2)
+end;
+begin
+end.
---
+++ дкр 7.pas
@@ -0,0 +1,28 @@
+﻿Uses  GraphABC, A;
+var (m,wx,wy,g) := (100, 300,300,0);
+ 
+Procedure KeyDown(k: integer);
+begin
+ case K of
+    VK_Down: wy += 10;
+    VK_Up: wy -= 10; 
+    VK_Left: wx -= 10;  
+    VK_Right: wx += 10;
+    VK_Escape: halt(1); //выход
+    VK_A: if m < 500 then m+=10; // + маштаб
+    VK_Z: if m > 20 then m-=10; // - маштаб
+    VK_S: if g < 15 then g+=1; // + глубина
+    VK_X: if g > 2 then g -=1; // - глубина
+   end;
+ Window.Clear; 
+ ris(wx-m,wy,wx+m,wy,g);
+ Redraw;
+end; 
+ 
+Begin
+     SetWindowCaption('Фракталы: Кривая Дракона');
+     SetWindowSize(700,512);
+     LockDrawing;
+     KeyDown(0);
+     OnkeyDown += KeyDown;
+End.
